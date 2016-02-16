f0fc33b0ed0930499d1f207fe743210acb98e640
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2016-02-16 15:52:42 -0800

    no message

--- Flag Table/FlagViewController.swift
+++ Flag Table/FlagViewController.swift
@@ -9,12 +9,11 @@
 import UIKit
 
 class FlagViewController: UIViewController {
+    
+    @IBOutlet weak var flagImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
-
-
 }
